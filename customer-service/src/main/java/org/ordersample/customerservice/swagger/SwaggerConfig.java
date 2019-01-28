299e6de3e2cb4f740c6051f7ee80f5f6b58dd944
System Administrator <root@CAVENDANOA.local>
2019-01-28 15:38:22 -0500

    prometheus - zipkin(check it out) - logging

--- customer-service/src/main/java/org/ordersample/customerservice/swagger/SwaggerConfig.java
+++ customer-service/src/main/java/org/ordersample/customerservice/swagger/SwaggerConfig.java
@@ -10,21 +10,36 @@ import org.springframework.web.context.request.async.DeferredResult;
 import com.fasterxml.classmate.TypeResolver;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.WildcardType;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+	@Autowired
+	private ServletContext servletContext;
+
 	@Bean
 	  public Docket api() {
 	    return new Docket(DocumentationType.SWAGGER_2)
 	            .select()
-	            .apis(RequestHandlerSelectors.basePackage("org.ordersample.customerservice"))
-	            .build()
+	            	.apis(RequestHandlerSelectors.basePackage("org.ordersample.customerservice"))
+	            	.build()
+				//.apiInfo(apiInfo())
 	            .pathMapping("/")
+				/*.host("http://localhost:8080/")
+				.pathProvider(new RelativePathProvider(servletContext){
+					@Override
+					public String getApplicationBasePath(){
+						return "/myapi";
+					}
+				})*/
 	            .genericModelSubstitutes(ResponseEntity.class, CompletableFuture.class)
 	            .alternateTypeRules(
 	                    newRule(typeResolver.resolve(DeferredResult.class,
@@ -33,6 +48,19 @@ public class SwaggerConfig {
 	            )
 	            .useDefaultResponseMessages(false);
 	  }
+
+	private ApiInfo apiInfo(){
+
+		ApiInfo apiInfo = new ApiInfo("DE-Insights",
+				"Insights' description",
+				"Version 0.1",
+				"terms Of Service Url",
+				"CARLOS",
+				"license",
+				"license Url");
+
+		return apiInfo;
+	}
 
 	  @Autowired
 	  private TypeResolver typeResolver;
